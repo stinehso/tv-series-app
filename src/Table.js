9adda9239f41eaa66b150b8e272e6c7b48afa58f
Stine <stinehso@gmail.com>
2020-08-29 00:39:28 +0200

    Fix layout

--- src/Table.js
+++ src/Table.js
@@ -5,38 +5,37 @@ function Table({ headers, shows }) {
 
     return (
         <div className="pa4">
-            <div className="overflow-auto">
-                <table className="f6 w-100 mw8 center" cellSpacing="0">
-                <thead>
-                    <tr>
+          <div className="overflow-auto">
+            <table className="f6 w-100 mw8 center" cellSpacing="0">
+              <thead>
+                <tr>
+                  {
+                    headers.map((heading, i) => {
+                      return (
+                        <th className="fw6 bb b--black-20 tl pb3 pr3 bg-white tc"
+                            id={`header${i}`}>{heading}</th>
+                      )
+                    })
+                  }
+                </tr>
+              </thead>
+              <tbody className="lh-copy">
+                {
+                  shows.map((show, i) => {
+                    return (
+                      <tr>
                         {
-                            headers.map((heading, i) => {
-                                return (
-                                    <th className="fw6 bb b--black-20 tl pb3 pr3 bg-white tc"
-                                        id={`header${i}`}>{heading}</th>
-                                )
-                            })
+                          Object.keys(show).map((keyName, i) => (
+                            <td className="pv3 pr3 bb b--black-20 tc">{show[keyName]}</td>
+                          ))
                         }
-                    </tr>
-                </thead>
-                <tbody className="lh-copy">
-                    {
-                        shows.map((show, i) => {
-                            return (
-                                <tr>
-                                {
-                                    Object.keys(show).map((keyName, i) => (
-                                        <td className="pv3 pr3 bb b--black-20 tc">{show[keyName]}</td>
-                                    ))
-                                }
-                                </tr>
-                            )
-                            
-                        })
-                    }
-                </tbody>
-                </table>
-            </div>
+                      </tr>
+                    )    
+                  })
+                }
+              </tbody>
+            </table>
+          </div>
         </div>
     )
 }
